997654dad5aa3a7d759c7295474215c3465671eb
Kunihiro Ando <senna5150ando@gmail.com>
2021-07-28 08:40:50 +0900

    commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -17,11 +17,40 @@ window.addEventListener("load",function(){
           FS.createDataFile(path, filename, xhr.response, true, true);
           var startCore = Module.cwrap("startEmulator", "number", ["number"]);
           startCore(0);
+          resizeCanvas();
         }
       }
     };
     xhr.open("GET", url, true);
     xhr.send();
   });
+  window.addEventListener(
+    "resize",
+    (e) => {
+      resizeCanvas();
+    },
+    true
+  );
 })
+
+const resizeCanvas = () => {
+  setTimeout(() => {
+    let canvas = document.getElementById("canvas");
+    const wh = window.innerHeight;
+    const ww = window.innerWidth;
+    const nw = 640;
+    const nh = 480;
+    const waspct = ww / wh;
+    const naspct = nw / nh;
+
+    if (waspct > naspct) {
+      var val = wh / nh;
+    } else {
+      var val = ww / nw;
+    }
+    let ctrldiv = document.querySelector(".ctrl_div");
+    canvas.style.height = 480 * val - ctrldiv.offsetHeight - 18 + "px";
+    canvas.style.width = 640 * val - 24 + "px";
+  }, 1200);
+};
 
